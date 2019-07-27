f79e53dd1c92ab876403a279a61df0af5e4a9d47
jos3duardo <jos3duardolopes@gmail.com>
2019-07-27 14:52:08 -0400

    colocando botoes na tela inicia

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.dashboard')
 @section('content')
 <div class="container-fluid">
+    <a href="{{route('category')}}" class="btn btn-dark btn-sm">Categoria</a>
+    <a href="#" class="btn btn-dark btn-sm">Produtos</a>
+    <a href="#" class="btn btn-dark btn-sm">Estoque</a>
 
 </div>
 @endsection
